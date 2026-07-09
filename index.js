6a721aa277efd354dfd4126254260fca94c08ffe
Azzam Uddin <uazzam69@gmail.com>
2026-07-10 03:45:26 +0530

    updated version

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 app.get("/", (req, res) => {
-    return res.send("version 2");
+    return res.send("version 3");
 });
 app.listen(4321, () =>{
 	console.log("app is running on port 4321");
